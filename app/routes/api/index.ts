9e1559e7d7da1f647bb9658e1ccbdac78ad76b28
dongwon kang <dw3624@gmail.com>
2024-06-14 18:32:02 +0900

    feat: 버킷 연결

--- app/routes/api/index.ts
+++ app/routes/api/index.ts
@@ -1,51 +1,105 @@
 import { Hono } from 'hono';
-import { getAuthorById, getAuthors, getBookById, getBooks } from '../../db';
 import { drizzle } from 'drizzle-orm/d1';
-import { author, book } from '../../../schema';
+import { author, book } from '../../../db/schema';
+import { eq, ilike, or } from 'drizzle-orm';
 
 const app = new Hono();
 
-app.post('/books', async (c) => {
-  return;
-});
-
-app.get('/books', async (c) => {
-  const db = drizzle(c.env.D1);
-  const result = await db.select().from(book).all();
-  console.log(result);
-  return c.json({ data: result });
-});
-
-app.get('/books/:id', async (c) => {
-  const id = c.req.param('id');
-  const book = await getBookById(c.env.D1, +id);
-  return c.json({
-    book,
-  });
-});
-
-app.get('/books/:id/content', async (c) => {
-  const id = c.req.param('id');
-  return c.json({
-    msg: 'hello',
-  });
-});
-
 app.get('/authors', async (c) => {
   const initial = c.req.query('initial');
-  const authors = await getAuthors(c.env.D1, initial);
+  const db = drizzle(c.env.D1);
+  const result = await db
+    .select()
+    .from(author)
+    .where(initial ? eq(author.initial, initial) : undefined);
   return c.json({
-    length: authors.length,
-    data: authors,
+    length: result.length,
+    data: result,
   });
 });
 
 app.get('/authors/:id', async (c) => {
   const id = c.req.param('id');
-  const author = await getAuthorById(c.env.D1, +id);
+  const db = drizzle(c.env.D1);
+  const result = await db
+    .select()
+    .from(author)
+    .where(eq(author.id, +id))
+    .leftJoin(book, eq(book.authorId, author.id));
+  return c.json({ data: result[0] });
+});
+
+app.post('/books', async (c) => {
+  const body = await c.req.formData();
+  const db = drizzle(c.env.D1);
+
+  return;
+});
+
+app.get('/books', async (c) => {
+  const initial = c.req.query('initial');
+  const db = drizzle(c.env.D1);
+  const result = await db
+    .select()
+    .from(book)
+    .where(initial ? eq(book.initial, initial) : undefined);
+  return c.json({ data: result });
+});
+
+app.get('/books/:id', async (c) => {
+  const id = c.req.param('id');
+  const db = drizzle(c.env.D1);
+  const result = await db
+    .select()
+    .from(book)
+    .where(eq(book.id, +id))
+    .leftJoin(author, eq(book.authorId, author.id));
+  return c.json({ data: result[0] });
+});
+
+app.get('/books/:id/content', async (c) => {
+  const id = c.req.param('id');
+  const db = drizzle(c.env.D1);
+  const bucket = c.env.R2;
+  const keyInfo = await db
+    .select({
+      title: book.title,
+      author: author.name,
+    })
+    .from(book)
+    .where(eq(book.id, +id))
+    .leftJoin(author, eq(book.authorId, author.id));
+
+  if (keyInfo.length === 0) {
+    return c.json({ error: 'Book not found' }, 404);
+  }
+
+  const path = `${keyInfo[0].author}_${keyInfo[0].title}.md`;
+  const object = await bucket.get(path);
+
+  if (!object) {
+    return c.json({ error: 'Content not found' }, 404);
+  }
+
+  const content = await object.text();
+
   return c.json({
-    data: author,
+    data: { title: keyInfo[0].title, author: keyInfo[0].author, content },
   });
+});
+
+app.get('/search/author', async (c) => {
+  const keyword = c.req.query('keyword');
+  const db = drizzle(c.env.D1);
+  if (!keyword) {
+    return;
+  }
+  const result = await db
+    .select()
+    .from(author)
+    .where(ilike(author.name, keyword));
+
+  return c.json({ data: result });
 });
 
 export default app;
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './schema.ts',
+  schema: './db/schema.ts',
   out: './migrations',
   dialect: 'sqlite',
   driver: 'd1-http',
