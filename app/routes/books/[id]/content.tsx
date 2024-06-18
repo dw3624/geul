82916843b1467ac474de146324abf9de9953f17d
dongwon kang <dw3624@gmail.com>
2024-06-19 07:35:33 +0900

    feat: 일부 수정

--- app/routes/books/[id]/content.tsx
+++ app/routes/books/[id]/content.tsx
@@ -5,7 +5,7 @@ import { marked } from 'marked';
 export default createRoute(async (c) => {
   const { id } = c.req.param();
   const { book, author } = await findBookById(c.env.D1, +id);
-  const path = `${author.name}_${book.title?.replaceAll(' ', '')}`;
+  const path = `${author?.name}_${book.title?.replaceAll(' ', '')}`;
   const content = (await findContent(c.env.R2, path)) || '';
   const html = await marked(content, { breaks: true });
   const view = await updateBookById(c.env.D1, +id);
@@ -14,7 +14,9 @@ export default createRoute(async (c) => {
     <section class="py-10 lg:py-10 space-y-12">
       <header class="text-center font-serif">
         <div class="text-3xl font-bold leading-9">{book.title}</div>
-        <div class="mt-8 text-lg font-semibold">{author?.name}</div>
+        <div class="mt-8 text-lg font-semibold">
+          {author?.name || '작자 미상'}
+        </div>
       </header>
       <div class="pt-8 pb-80">
         <article
